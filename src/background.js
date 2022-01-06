b614251d61c5a65d994cbfee2493df608b8b594c
Farsad <fakhim.farsad@gmail.com>
2022-01-06 21:38:13 +0100

    Boilerplate

---
+++ src/background.js
@@ -0,0 +1,8 @@
+// browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+browser.runtime.onMessage.addListener(() => {
+  console.log('Hello from the background');
+
+  browser.tabs.executeScript({
+    file: 'content-script.js',
+  });
+});
